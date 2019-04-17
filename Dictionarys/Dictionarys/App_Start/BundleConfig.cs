c5e07ae5ce8378cfe8b12812493e2c8b182b5ab6
Brenner Hernández <velasquezmochis@gmail.com>
2019-04-16 23:05:25 -0600

    Added lines to BundleC

--- Dictionarys/Dictionarys/App_Start/BundleConfig.cs
+++ Dictionarys/Dictionarys/App_Start/BundleConfig.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
+using System.Web.Optimization;
 
-namespace Dictionarys.App_Start
+namespace Dictionarys
 {
     public class BundleConfig
     {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                "~/Scripts/site.js")
+            );
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                 "~/Content/menu.css",
+                 "~/Content/site.css"));
+        }
     }
 }
